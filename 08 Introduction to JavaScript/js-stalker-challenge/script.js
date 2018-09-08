f6faba9a2d0d3914f0da4bdb62759e89fd06429d
Tim Sullivan <timsullivan97@gmail.com>
2018-09-07 22:03:43 -0700

    adding js user input challenge

---
+++ 08 Introduction to JavaScript/js-stalker-challenge/script.js
@@ -0,0 +1,13 @@
+// Ask for the user's first name
+let userName = prompt("What is your first name breh?");
+
+// Ask for the user's last name
+let lastName = prompt("How about that last name doe?");
+
+// Print out the user's full name in a sentence
+alert("That's lit bro. Your name is " + userName + " " + lastName);
+
+// Print out the user's age in a sentence
+let userAge = prompt("How about that age?");
+
+alert("You are " + userAge + " years old, really?");
